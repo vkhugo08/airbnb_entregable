ea135c78008f4fbe32eaf385d4297bd15bf5f7d3
Victor Medrano <vkmedrano@hotmail.com>
2022-09-15 12:44:05 -0400

    resubiendo

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,14 +1,22 @@
 const { Sequelize } = require('sequelize');
-const { PORT } = require('../app')
-
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 // Create a connection to database
-const db = new Sequelize( {
+const db = new Sequelize({
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'apache421',
-  database: 'Airbnb',
-
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB,
+  logging: false,
+  dialectOptions:
+    process.env.NODE_ENV === 'production'
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        }
+      : {},
 });
-
 module.exports = { db };
